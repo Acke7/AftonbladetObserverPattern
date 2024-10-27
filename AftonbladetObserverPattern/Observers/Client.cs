a943555559d99d99c13f66f7366bb52e64eed0b1
acke7 <ahmadaloalbi@ouylook.com>
2024-10-27 16:55:08 +0100

    Add project files.

---
+++ AftonbladetObserverPattern/Observers/Client.cs
@@ -0,0 +1,22 @@
+﻿using AftonbladetObserverPattern.Subject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AftonbladetObserverPattern.Observers
+{
+    internal class Client: IObserver
+    {
+        public string Name { get; set; }
+        public Client(string name)
+        {
+            Name = name;
+        }
+        public void Update(Publisher pub)
+        {
+            Console.WriteLine("Linda: Thanks for the story.");
+        }
+    }
+}
---
+++ AftonbladetObserverPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using AftonbladetObserverPattern.Observers;
+using AftonbladetObserverPattern.Subject;
+
+namespace AftonbladetObserverPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var publisher = new Publisher();
+            var observerA = new Client("Ahmad"); 
+            publisher.Attach(observerA);
+
+            var observerB = new Client("alice");
+            publisher.Attach(observerB);
+
+            var observerC = new Client("adsad");
+            publisher.Attach(observerC);
+
+            // Övning 1
+            publisher.ReleaseNewMaterial();
+
+            // Övning 2
+            publisher.Detach(observerC);
+
+            // Övning 3
+         
+
+            // Övning 4
+            publisher.ReleaseNewMaterial();
+        }
+    }
+}
